4e730cc269b3270d95861ce766152555d5d40c8c
CCow <44686625+CCow@users.noreply.github.com>
2022-03-02 23:47:46 -0800

    Add files via upload
    added lines #36 and #51 in the grammar to accept new rules for the hello.cc file and parse successfully

---
+++ hello.cc
@@ -0,0 +1,11 @@
+// a small C++ program
+#include <iostream>
+
+int main()
+{
+  	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
+
+//Cpp.cf line #36 parses qualifying Id's (hello.cc "::")
+//Cpp.cf line #51 is rule for parsing "<<" within an expression
